e53a90f259bda87bbcef90d1192050c594afe882
feiernanduo22 <feiernanduo22@163.com>
2020-06-27 23:35:36 +0800

    wj项目 前段拦截器

---
+++ src/main/java/com/evan/wj/error/ErrorConfig.java
@@ -0,0 +1,22 @@
+package com.evan.wj.error;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * ErrorConfig class
+ *
+ * @auther Yvqanlee
+ * @data 2020/6/27 23:29
+ */
+@Component
+public class ErrorConfig implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+        registry.addErrorPages(error404Page);
+    }
+}
